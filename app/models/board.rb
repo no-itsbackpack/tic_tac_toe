3abb46f74e611970bca6e1b82e150e5da457077e
egomez <egomez@egomez.local>
2013-02-08 15:47:26 -0600

    Added some model infrastructure

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,4 @@
 class Board < ActiveRecord::Base
+  has_many :positions
   # attr_accessible :title, :body
 end
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,3 +1,4 @@
 class Position < ActiveRecord::Base
-  # attr_accessible :title, :body
+  belongs_to :board
+  attr_accessible :row, :col, :player
 end
